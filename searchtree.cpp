c136678264b93068a5b213a910546e98a0527aae
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-06-09 15:27:15 +0700

    step2

--- searchtree.cpp
+++ searchtree.cpp
@@ -9,4 +9,12 @@ public:
     node *leftchild;
     node *rightchild;
 
-   
+    // Constructor for the node class
+    node(string i, node *l, node *r)
+    {
+        info = i;
+        leftchild = l;
+        rightchild = r;
+    }
+};
+
